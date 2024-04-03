8223a53475d1e79bc6e4f66fc9fd63db28dda532
Meshmanuu <meshanyangu@gmail.com>
2024-04-03 15:22:27 +0300

    lab done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('text').textContent = 'This is really cool!';
+  });
+  
 });
